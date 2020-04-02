8f54cd791dbf549f381eb16f8fe899aad331d200
Madhav Sharma <madhav.sharma@okstate.edu>
2020-04-01 22:39:39 -0500

    Added Firebase and CocoaPods

--- FinalProjectMSIS/AppDelegate.swift
+++ FinalProjectMSIS/AppDelegate.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 //this is a test to push to github. 
 
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-FinalProjectMSIS-FinalProjectMSISUITests/Pods-FinalProjectMSIS-FinalProjectMSISUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FinalProjectMSIS_FinalProjectMSISUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FinalProjectMSIS_FinalProjectMSISUITests
+@end
