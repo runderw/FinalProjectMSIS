49836c26a36d24c38a2ffc565f2abf2758349c39
Madhav Sharma <madhav.sharma@okstate.edu>
2020-04-15 19:05:29 -0500

    Declared database variables.

--- FinalProjectMSIS/ViewController.swift
+++ FinalProjectMSIS/ViewController.swift
@@ -8,17 +8,41 @@
 
 import SwiftUI
 import Firebase
+import FirebaseCore
+import FirebaseFirestore
+
+
+
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let db = Firestore.firestore()
-        
+          let db = Firestore.firestore()
         db.collection("Attendance").document("PWC").setData(["test":"test"])
         
     }
+}
+
+public struct Attendance: Codable {
+
+    
+    let CWID: String
+          //set as string in case someone puts an A.
+    let Name: String
+    let Code: String
+    let Company: String
+          
+
+    enum CodingKeys: String, CodingKey {
+        case CWID
+        case Name
+        case Code
+        case Company
+        
+    }
+
 }
 
 
